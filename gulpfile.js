306fe46b3b7f5779f27633322bc9269b82b8a3e5
Szymon <kierej@gmail.com>
2017-07-07 22:38:18 +0200

    prepared project directory to work

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,6 @@ var pug = require('gulp-pug');
 var browserSync = require('browser-sync');
 var fs = require('fs-extra');
 
-var paths = {
-  scss: './stylesheets/*.scss'
-};
-
 // delete build directory
 gulp.task('clean', function() {
   fs.removeSync('./build');
@@ -16,27 +12,36 @@ gulp.task('clean', function() {
 
 //copy JS directory
 gulp.task('copyJS', function(){
-  var src = "./dev/app";
-  var dist = "./build/app";
+  var src = "./dev/assets/app";
+  var dist = "./build/assets/app";
   fs.copySync(src, dist);
 })
 
+//copy img directory
+gulp.task('copyImg', function(){
+  var src = "./dev/assets/img";
+  var dist = "./build/assets/img";
+  fs.copySync(src, dist);
+})
+
+
 // sass
 gulp.task('sass', function() {
-  return gulp.src('./dev/stylesheets/*.scss')
+  return gulp.src('./dev/assets/stylesheets/**/*.scss')
     .pipe(sass({
         outputStyle: 'expanded',
         errLogToConsole:true,
         includePaths: [
           './',
-          './node_modules/foundation-sites/scss',
-          './dev',
-          './dev/stylesheets'
-        ],
-        loadPath: ['node_modules/foundation-sites/scss']
+          './dev/assets/stylesheets',
+          './node_modules/normalize-scss/sass',          
+        ]
     }))
-            
-    .pipe(gulp.dest('./build/stylesheets/'));
+    .on('error', function(errorInfo){
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })            
+    .pipe(gulp.dest('./build/assets/stylesheets/'));
 });
 
 // pug
@@ -51,7 +56,7 @@ gulp.task('pug', function() {
 
 // browserSync
 gulp.task('browser-sync', function(){
-  browserSync.init(['build/stylesheets/*.css', 'dev/app/*.js', 'build/app/*.js', 'build/index.html'],
+  browserSync.init(['build/assets/stylesheets/*.css', 'dev/assets/app/**/*.js', 'build/assets/app/*.js', 'build/index.html'],
     {
       server: {
         baseDir: "./build/"
@@ -60,8 +65,9 @@ gulp.task('browser-sync', function(){
 });
 
 // watch
-gulp.task('watch', ['clean', 'copyJS','sass', 'pug', 'browser-sync'], function(){
-  gulp.watch(['./dev/stylesheets/**/*.scss'], ['sass']);
+gulp.task('watch', ['clean', 'copyJS', 'copyImg','sass', 'pug', 'browser-sync'], function(){
+  gulp.watch(['./dev/assets/stylesheets/**/*.scss'], ['sass']);
   gulp.watch(['./dev/*.pug'], ['pug'] );
-  gulp.watch(['./dev/app/*.js'], ['copyJS'])
+  gulp.watch(['./dev/assets/app/*.js'], ['copyJS']);
+  gulp.watch(['./dev/assets/img/*.*'], ['copyImg'])
 })
