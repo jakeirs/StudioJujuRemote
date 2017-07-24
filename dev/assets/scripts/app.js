b7ed87abaee8e38924b2962253aa74ae9245cd18
Szymon <kierej@gmail.com>
2017-07-25 00:56:34 +0200

    added sticky menu

--- dev/assets/scripts/app.js
+++ dev/assets/scripts/app.js
@@ -1,16 +1,18 @@
-class RevealOnScroll {
-  constructor() {
-    this.itemsToReveal = document.querySelectorAll(".wrapper");
-    this.hideInitially();
-  }
+import ToggleClassOnScroll from './modules/ToggleClassOnScroll';
+import SetHeight from './modules/SetHeight';
+import waypoints from '../../../node_modules/waypoints/lib/noframework.waypoints';
 
-  // hideInitially() {
-  //   return "elo";
-  //   // this.itemsToReveal.
-  // }
-  
-}
+document.addEventListener('DOMContentLoaded', function(event) {
+  // Toggled class in header
+  var logoAnimated = new ToggleClassOnScroll( '.logo-navbar', 'logo-navbar--is-visible', '0' ),
+      navbarSticky = new ToggleClassOnScroll( '#navbar', 'navbar--fixed', '0' ),
+      navRemovePadding = new ToggleClassOnScroll( '#nav', 'nav--no-padding', '0' );
 
-let tt = 43431231243;
+// set height to .wrap-navbar prevent immediatelly fill the gap after #navbar gets sticky      
+var setHeightWrapNavbar = new SetHeight('.wrap-navbar');
+})
 
-console.log(`${tt}  no no no `);
+
+
+
+
